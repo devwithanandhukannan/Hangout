770a5d58c08ccf5f5a64ee938e12084eb32e97ee
Anandhu <anandhu@Anandhus-MacBook-Air.local>
2026-05-18 22:52:42 +0530

    update backend api endpoint

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,11 @@ import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const API_BASE_URL = process.env.VITE_API_BASE_URL || "https://hangout-all4.onrender.com/";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -15,26 +20,18 @@ export default defineConfig({
     },
   },
   server: {
-    // host: "0.0.0.0",
     port: 5173,
-
-    // allowedHosts: [
-    //   ".trycloudflare.com"
-    // ],
-
-    
     proxy: {
-  "/api": {
-    target: "https://hangout-all4.onrender.com/",
-    changeOrigin: true,
-    rewrite: (path) => path.replace(/^\/api/, ""),
-  },
-
-  "/socket.io": {
-    target: "https://hangout-all4.onrender.com/",
-    changeOrigin: true,
-    ws: true
-  }
-}
+      "/api": {
+        target: API_BASE_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+      "/socket.io": {
+        target: API_BASE_URL,
+        changeOrigin: true,
+        ws: true,
+      },
+    },
   },
 });
